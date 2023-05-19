57162ee1fc72cf2eebbe09c7a5eb2ed7df2f3e11
dmedelacruz <dmedelacruz10@gmail.com>
2023-05-19 22:34:36 +0800

    init

---
+++ src/main/java/com/formedix/dmedelacruz/FormedixDmedelacruzApplication.java
@@ -0,0 +1,13 @@
+package com.formedix.dmedelacruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormedixDmedelacruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormedixDmedelacruzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/formedix/dmedelacruz/FormedixDmedelacruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formedix.dmedelacruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormedixDmedelacruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
